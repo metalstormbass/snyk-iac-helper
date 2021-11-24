c7e346d1eb9e98c78b65f634935ceb0e43c56831
mike <michaelbraunbass@gmail.com>
2021-11-24 09:36:50 -0700

    Updated Output

--- snyk-iac-helper.sh
+++ snyk-iac-helper.sh
@@ -5,8 +5,8 @@ ORANGE='\033[1;33m'
 CYAN='\033[1;36m'
 GRAY='\033[1;30m'
 BLUE='\033[1;34m'
-PURPLE='\033[0;35m'
-LRED='\033[0;31m'
+PURPLE='\033[1;35m'
+BLUE='\033[1;34m'
 NC='\033[0m' 
 
 if [ -e snyk_iac_results.json ]
@@ -51,14 +51,24 @@ for i in $(seq 0 $RESULT); do
             then 
             :
         else
-            echo "${PURPLE}>>>>>>>> Issue <<<<<<<<${NC}"
-            echo ${LRED}$ISSUE${NC}; \
+            #Title
+            TITLE=`cat snyk_iac_results.json| jq '.['$i'] | .infrastructureAsCodeIssues | select(length > 0)' | jq '.['$j'].title';`
+            echo "${PURPLE}>>>>>>>> $TITLE <<<<<<<<${NC}"
+            #Issue
+            printf "Issue: "
+            echo ${BLUE}$ISSUE${NC}; \
             
+            #Snyk ID
+            printf "SNYK ID: "
+            SNYKID=`cat snyk_iac_results.json | jq '.['$i'] | .infrastructureAsCodeIssues | select(length > 0)' | jq '.['$j'].publicId';`
+            echo $SNYKID          
+
+            #Line Number
             printf "Line Number: "
-            #cat snyk_iac_results.json | jq '.['$i'] | .infrastructureAsCodeIssues | select(length > 0)' | jq '.['$j'].lineNumber'; \
             LINENUMBER=`cat snyk_iac_results.json | jq '.['$i'] | .infrastructureAsCodeIssues | select(length > 0)' | jq '.['$j'].lineNumber';`
             echo $LINENUMBER
 
+            #Severity
             printf "Severity: "
             SEVERITY=`cat snyk_iac_results.json | jq '.['$i'] | .infrastructureAsCodeIssues | select(length > 0)' | jq '.['$j'].severity';` 
             
@@ -72,21 +82,26 @@ for i in $(seq 0 $RESULT); do
                 echo ${GRAY}$SEVERITY${NC}
             fi
 
-
-
+            #Impact
             printf  "Impact: " 
             IMPACT=`cat snyk_iac_results.json| jq '.['$i'] | .infrastructureAsCodeIssues | select(length > 0)' | jq '.['$j'].impact';` 
             echo $IMPACT
 
-
-            echo "${LRED}Affected line or block in $FILE${NC}:\n"
+            #Affected Line
+            echo "Affected line or block in ${BLUE}$FILE${NC}:\n"
             eval sed -n "$LINENUMBER"p $FILENAME  
             printf "\n"
 
+            #Resolve
             printf  "Resolve: "
             RESOLVE=`cat snyk_iac_results.json| jq '.['$i'] | .infrastructureAsCodeIssues | select(length > 0)' | jq '.['$j'].resolve';` 
             echo $RESOLVE
-            printf "\n"
+
+            #References
+            printf "Documentation: "
+            DOCUMENTATION=`cat snyk_iac_results.json | jq '.['$i'] | .infrastructureAsCodeIssues | select(length > 0)' | jq '.['$j'].documentation';`
+            printf $DOCUMENTATION
+            printf "\n\n"
         fi    
     done; \
     echo " ------------------------------------------------------------------------ \n" ; \
